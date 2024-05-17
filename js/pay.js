4b0da5a130414f6eb961e5a4332f1f30ae762f47
Yauheni <37824172+YauheniRusinchuk@users.noreply.github.com>
2024-05-17 16:46:22 +0300

    Update pay.js

--- js/pay.js
+++ js/pay.js
@@ -1,30 +1,10 @@
 window.document.addEventListener("DOMContentLoaded", () => {
-  const container = document.getElementsByClassName("container")[0];
-  const acsBlock = document.getElementById("acs");
-  const iframe = document.getElementById("acsFrame");
+  // const container = document.getElementsByClassName("container")[0];
+  // const acsBlock = document.getElementById("acs");
+  // const iframe = document.getElementById("acsFrame");
   const progress = document.getElementById("progressNew");
-  const main = document.getElementsByClassName('main')[0]
 
   progress.style.display = "none";
-
-  // const params = new URLSearchParams(window.location.search);
-
-  // const back_url = params.get("back_url");
-
-  // if (back_url) {
-  //   progress.style.display = "none";
-  //   window.location.href = back_url; 
-  // }
-
-  if (iframe) {
-    iframe.style.height = "100vh";
-    iframe.style.maxWidth = "100%";
-    iframe.style.maxHeight = "100%";
-    acsBlock.style.margin = "0";
-
-  // console.log("container :", container);
-    container.style.paddingBottom = "0";
-    container.style.marginBottom = "0";
-  }
   
 });
+
