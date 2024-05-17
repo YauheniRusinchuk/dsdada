4c8028c82183b7ea67c29c3662d23271cbd5e6ac
Yauheni <37824172+YauheniRusinchuk@users.noreply.github.com>
2024-05-17 18:26:36 +0300

    Update pay.js

--- js/pay.js
+++ js/pay.js
@@ -1,10 +1,21 @@
-window.document.addEventListener("DOMContentLoaded", () => {
-  // const container = document.getElementsByClassName("container")[0];
-  // const acsBlock = document.getElementById("acs");
-  // const iframe = document.getElementById("acsFrame");
+window.onload = function() {
+
   const progress = document.getElementById("progressNew");
+  // const result = document.querySelector('#resultCardPage');
+  const urlParams = new URLSearchParams(window.location.search);
+  const back_url = urlParams.get('back_url');
 
   progress.style.display = "none";
-  
-});
 
+  const observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+          if (mutation.target.style.display !== 'none') {
+              window.location.href = back_url;
+          }
+      });
+  });
+
+  const config = { attributes: true };
+  const target = document.querySelector('#resultCardPage');
+  observer.observe(target, config);
+};
