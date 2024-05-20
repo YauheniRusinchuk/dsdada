b6af48da94a50b692fc0d0087ccc55c1ef42129e
Yauheni <37824172+YauheniRusinchuk@users.noreply.github.com>
2024-05-20 14:31:14 +0300

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,6 +1,7 @@
 var settings = {
   pageType: "reg",
-  expiredCardsAllowed: true
+  expiredCardsAllowed: true,
+  redirectTimeout: 3
 };
 
 var API_URL = 'https://www.pga.gazprombank.ru/api/v4/4246218C81AF2F5E6DF9742C494ADF22/';
