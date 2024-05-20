f8d10692753912fc596161f5b6b16dcd0644edc0
Yauheni <37824172+YauheniRusinchuk@users.noreply.github.com>
2024-05-20 13:08:22 +0300

    Update pay.js

--- js/pay.js
+++ js/pay.js
@@ -1,21 +1,44 @@
+// window.document.addEventListener("DOMContentLoaded", () => {
+//   // const container = document.getElementsByClassName("container")[0];
+//   // const acsBlock = document.getElementById("acs");
+//   // const iframe = document.getElementById("acsFrame");
+//   const progress = document.getElementById("progressNew");
+
+//   progress.style.display = "none";
+  
+// });
+
+
 window.onload = function() {
+
+  const iframeDocument = document.querySelector('iframe').contentDocument || document.querySelector('iframe').contentWindow.document;
 
   const progress = document.getElementById("progressNew");
   // const result = document.querySelector('#resultCardPage');
-  const urlParams = new URLSearchParams(window.location.search);
-  const back_url = urlParams.get('back_url');
+  // const urlParams = new URLSearchParams(window.location.search);
+  // const back_url = urlParams.get('back_url');
 
   progress.style.display = "none";
 
   const observer = new MutationObserver(function(mutations) {
-      mutations.forEach(function(mutation) {
-          if (mutation.target.style.display !== 'none') {
-              window.location.href = back_url;
-          }
+    mutations.forEach(function(mutation) {
+        if (mutation.type === 'childList') {
+          mutation.addedNodes.forEach(function(addedNode) {
+            if (addedNode.tagName === 'DIV' && addedNode.id === 'resultCardPage') {
+              // Выполнить редирект, если добавлен div с id 'my-id'
+              const style = getComputedStyle(addedNode);
+              if (style.display !== 'none') {
+                // Выполнить редирект, если добавлен видимый div с id 'my-id'
+                window.location.href('https://google.com');
+              }
+
+            }
+          });
+        }
       });
   });
 
-  const config = { attributes: true };
-  const target = document.querySelector('#resultCardPage');
-  observer.observe(target, config);
+  const config = { attributes: true, childList: true, subtree: true };
+  // const target = document.querySelector('#resultCardPage');
+  observer.observe(iframeDocument, config);
 };
